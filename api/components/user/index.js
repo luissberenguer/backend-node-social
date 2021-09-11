21486d3e09b5894abcf0b92f3be6e046e054ff14
Luis Berenguer <luis01developer@gmail.com>
2021-09-11 10:32:40 +0200

    Add vercel json

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -1,5 +1,11 @@
 // const store = require('../../../store/mysql.js');
-const store = require('../../../store/remote-mysql');
 const ctrl = require('./controller');
+const config = require('../../../config');
 
+let store;
+if (config.remoteDB == true) {
+    store = require('../../../store/remote-mysql');
+} else {
+    store = require('../../../store/mysql');
+}
 module.exports = ctrl(store); // lo hemos convertido en una función
